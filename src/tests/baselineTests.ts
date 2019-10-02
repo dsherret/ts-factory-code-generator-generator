c1da2468cb6d0daa87ef5777b47f6fb25af9ac6f
David Sherret <dsherret@gmail.com>
2019-10-01 23:41:07 -0400

    chore: Add multiple ts versions to the tests.

--- src/tests/baselineTests.ts
+++ src/tests/baselineTests.ts
@@ -4,22 +4,34 @@ import { expect } from "chai";
 import { Project } from "ts-morph";
 import * as ts from "typescript-3.6.3";
 import { generateCode } from "../generateCode";
-import { generateFactoryCode } from "./baselines/code-generation.baseline";
+import { generateFactoryCode } from "./baselines/code-generation/typescript-3.6.3.baseline";
 
 describe(nameof(generateCode), () => {
-    it("should equal the baseline", () => {
+    it("should equal the baseline for 3.2.1", () => {
+        runBaseLineForPackage("typescript-3.2.1");
+    });
+
+    it("should equal the baseline for 3.6.3", () => {
+        runBaseLineForPackage("typescript-3.6.3");
+    });
+
+    it("should equal the baseline for @next", () => {
+        runBaseLineForPackage("typescript-next");
+    });
+
+    function runBaseLineForPackage(packageName: string) {
         // get generated code
-        const result = generateCode("typescript-3.6.3");
+        const result = generateCode(packageName);
 
         // ensure no diagnostics
         ensureNoDiagnostics(result);
 
         // compare
-        const specFileName = path.join(__dirname, "baselines/code-generation.baseline.ts");
+        const specFileName = path.join(__dirname, `baselines/code-generation/${packageName}.baseline.ts`);
         const specText = fs.readFileSync(specFileName, { encoding: "utf8" });
         fs.writeFileSync(specFileName, result, { encoding: "utf8" }); // overwrite
         expect(result).to.equal(specText);
-    });
+    }
 });
 
 describe(nameof(generateFactoryCode), () => {
@@ -31,7 +43,7 @@ describe(nameof(generateFactoryCode), () => {
         const result = `import * as ts from "typescript-3.6.3";\n\n` + generateFactoryCode(ts, languageFeaturesSourceFile);
 
         // ensure no diagnostics
-        // ensureNoDiagnostics(result);
+        ensureNoDiagnostics(result);
 
         // compare
         const specFileName = path.join(__dirname, "baselines/factory-code-generation.baseline.ts");
